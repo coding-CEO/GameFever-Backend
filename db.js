b395222d2a541573b152043f578048c5a5684107
unknown <pratikraut0000@gmail.com>
2020-10-18 13:52:12 +0530

    changed database

--- db.js
+++ db.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql");
 
 let db_con = mysql.createConnection({
-  host: "sql12.freemysqlhosting.net",
-  user: "sql12345846",
-  password: "s119eztPgW",
-  port: 3306,
-  database: "sql12345846",
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME,
+
   // host: "localhost",
   // user: "root",
   // password: "",
   // database: "pcommersedb",
+
   multipleStatements: true,
 });
 
