a51df068bcf7c97ddac5c29e83b0c28bf2bdf80b
unknown <pratikraut100000@gmail.com>
2020-12-02 18:34:29 +0530

    now image upload returns a url

--- uploadImages.js
+++ uploadImages.js
@@ -3,7 +3,9 @@ const cloudinary = require('cloudinary').v2;
 let imgUpload = (path) =>{
     return new Promise(async(resolve, reject) => {
         try {
-            return resolve(await cloudinary.uploader.upload(path));
+            let result = await cloudinary.uploader.upload(path);
+            console.log(result);
+            return resolve(result.secure_url);
         } catch (error) {
             return reject(error);
         }
